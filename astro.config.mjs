1ce01f00d75434a14896334a6e88929707b7b4eb
Ruchita Machale <rucha.machale2003@gmail.com>
2026-08-21 18:47:43 +0530

    Install and configure dynamic @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://siddhivinayakdesigners.in'
+  site: 'https://siddhivinayakdesigners.in',
+  integrations: [sitemap()]
 });
